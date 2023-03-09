a3fc91011b3895fcbb344bb6928ff80e9f7afa4e
mengcc666 <55180978+mengcc666@users.noreply.github.com>
2023-03-09 17:13:18 +0800

    First commit

---
+++ src/main/java/hnu/mengcc/agentpeerapp/AgentpeerappApplication.java
@@ -0,0 +1,13 @@
+package hnu.mengcc.agentpeerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentpeerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentpeerappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hnu/mengcc/agentpeerapp/AgentpeerappApplicationTests.java
@@ -0,0 +1,13 @@
+package hnu.mengcc.agentpeerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentpeerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
